aad69b5eb7b0fc2b77473e41b4d408c1a4339633
Anjali749 <93035128+Anjali749@users.noreply.github.com>
2021-12-23 04:25:44 +0530

    Commit

--- api/index.js
+++ api/index.js
@@ -42,7 +42,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 6000, () => {
     console.log("Backend is running.");
 });
     
