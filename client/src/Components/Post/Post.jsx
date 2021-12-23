0c213778d1b9f1c43a0d9098146dfce3226f7350
Anjali749 <93035128+Anjali749@users.noreply.github.com>
2021-12-23 08:06:17 +0530

    commit

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css"
 import {Link} from "react-router-dom"
 
 export default function Post({ post }) {
-    const PF = "http://localhost:5000/images/"; //PF=Public Folder
+    const PF = "http://localhost:7000/images/"; //PF=Public Folder
     return (
         <div className="post">
             {post.photo && (
